a165dc6bb73ffb6886e53ce95ec2a8b8cd0f440c
MarcoUnie <mmagecoyt@gmail.com>
2025-05-14 09:26:21 +0200

    test metrics

---
+++ tests/test_metrics.py
@@ -0,0 +1,22 @@
+import pytest
+from src.proceso import Proceso
+from src.metrics import calcular_metricas
+
+@pytest.fixture(autouse=True)
+def limpiar_pids():
+    Proceso.limpiar_pids()
+
+def test_metricas_calculadas_correctamente():
+    p1 = Proceso("P1", 4, 1)
+    p1.tiempo_inicio = 0
+    p1.tiempo_fin = 4
+
+    p2 = Proceso("P2", 3, 1)
+    p2.tiempo_inicio = 4
+    p2.tiempo_fin = 7
+
+    metricas = calcular_metricas([p1, p2])
+
+    assert metricas["respuesta_prom"] == pytest.approx(2.0)
+    assert metricas["retorno_prom"] == pytest.approx(3.5)
+    assert metricas["espera_prom"] == pytest.approx(0.5)
